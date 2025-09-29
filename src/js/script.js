6e90990bb52061e3b9e9665948e83f31cc9888f0
Youndoku <sahrurramadhan2004@gmail.com>
2025-09-30 00:51:31 +0700

    Change Background

--- src/js/script.js
+++ src/js/script.js
@@ -33,13 +33,12 @@ function checkLocation() {
         var radius = 50; // Meter
 
         // Hitung jarak antara lokasi pengguna dan pusat
-        var distance = 10;
-        // calculateDistance(
-        //   userLat,
-        //   userLng,
-        //   centerLat,
-        //   centerLng
-        // );
+        var distance = calculateDistance(
+          userLat,
+          userLng,
+          centerLat,
+          centerLng
+        );
 
         if (distance <= radius) {
           alertbox.render({
